7922812c2179068a232d430fd65f256f00ddf377
Nadav Wolfin <nadav.wolfin@gmail.com>
2016-09-09 21:13:19 +0300

    First Version: MongoDB + NodeJS REST API + MIDDLEWEAR - Working!

--- NodeJS/SmartReceipts/app.js
+++ NodeJS/SmartReceipts/app.js
@@ -4,24 +4,55 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var receipts = require('./routes/api/receipts');
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var MONGODB_URI = 'mongodb://localhost:27017/receipts';
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(MONGODB_URI, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var receipts = require('./routes/api/receipts');
 
 app.use('/', routes);
 app.use('/users', users);
--- NodeJS/SmartReceipts/routes/api/receipts.js
+++ NodeJS/SmartReceipts/routes/api/receipts.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var RECEIPTS_COLLECTION = "receipts";
 
-/* GET home page. */
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
 router.route('/send').post(function(req, res, next) {
-    var name = req.body.Name
-    var output = {"response": name};
-    res.end( JSON.stringify(output));
+    var db = req.db;
+    var newContact = req.body;
+    newContact.createDate = new Date();
+
+    if (!(req.body.firstName || req.body.lastName)) {
+        handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+    }
+
+    db.collection(RECEIPTS_COLLECTION).insertOne(newContact, function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to create new contact.");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
 });
 
 module.exports = router;
