23c6afc2d4dae702149b8f13f0f16cde7709178c
Nadav Wolfin <nadav.wolfin@gmail.com>
2016-09-10 14:07:23 +0300

    Added customer preview

--- NodeJS/SmartReceipts/routes/api/receipts.js
+++ NodeJS/SmartReceipts/routes/api/receipts.js
@@ -11,7 +11,7 @@ function handleError(res, reason, message, code) {
 router.route('/send').post(function(req, res, next) {
     var db = req.db;
     var newReceipt = req.body;
-    newReceipt.createDate = new Date();
+    newReceipt.DateTime = new Date();
 
 
     db.collection(RECEIPTS_COLLECTION).insertOne(newReceipt, function(err, doc) {
@@ -23,4 +23,29 @@ router.route('/send').post(function(req, res, next) {
     });
 });
 
+router.route('/customer/preview').post(function(req, res, next) {
+    var db = req.db;
+    var clientID = req.body.ClientID;
+    var startDate = req.body.StartDate;
+    var endDate = req.body.EndDate;
+    var filterType = req.body.FilterType;
+    var filterValue = req.body.FilterValue;
+
+    var query = {
+        "DateTime" : { $gte : new Date(startDate),  $lte : new Date(endDate)},
+        "Customer.Id": clientID
+    };
+    query["Business."+filterType] = filterValue;
+
+    console.log(query);
+    selectFildes = { _id: 1, Business: 1, TotalPrice : 1, DateTime : 1 };
+
+    db.collection(RECEIPTS_COLLECTION).find(query, selectFildes).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+});
 module.exports = router;
